65e9c9d334ba64dc7459bc392bd6cd19327a2439
NedNedov228 <nakhmedli@gmail.com>
2025-08-25 03:47:14 +0400

    Fixed error alert in auth page

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { isTokenExpired } from '../utils/jwtUtils';
 
 // Получаем значение переменной окружения и удаляем пробелы
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || ''
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || null
 
 
 console.log('API_BASE_URL:', API_BASE_URL);
